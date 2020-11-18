3831ff03e5fb5f96ce0fa64eb872e29e49d5b519
lelderbe <fvdc@ya.ru>
2020-11-19 01:16:11 +0300

    module User

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
-const os = require("os");
-const greeting = require("./greeting");
+const User = require("./user.js");
  
-// получим имя текущего пользователя
-let userName = os.userInfo().username;
- 
- 
-console.log(`Дата запроса: ${greeting.date}`);
-console.log(greeting.getMessage(userName));
+let eugene = new User("Eugene", 32);
+eugene.sayHi();
+eugene.displayInfo();
